756e77557f6fa7a9052da9906acfdad74ac661f2
fathirmarwanw-tech <fathirmarwanw@gmail.com>
2026-02-24 15:32:33 +0700

    PRAKTIKUM PBO

---
+++ App.java/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Nama: Fathir Marwan");
+        System.out.println("NIM: 105223044");
+    }
+}
